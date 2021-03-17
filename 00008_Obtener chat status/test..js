693b07b68ce63c87c5b0267f08d6409cff9cc4c2
mayra@mumuki.org <mayra@mumuki.org>
2021-03-17 16:17:34 +0000

    Mumuki Export on 2021-03-17 16:17:33 +0000

--- 00008_Obtener chat status/test..js
+++ 00008_Obtener chat status/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos obtenerChatStatus(['Ada'])  debería retornar 'Ada está conectada' ", function() {
+let resultado = obtenerChatStatus(['Ada'])
+assert.equal(resultado,'Ada está conectada' ,`obtenerChatStatus(['Ada'])  debería retornar 'Ada está conectada' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerChatStatus(['Ada', 'Grace'])  debería retornar 'Ada y Grace estan conectadas' ", function() {
+let resultado = obtenerChatStatus(['Ada', 'Grace'])
+assert.equal(resultado,'Ada y Grace están conectadas' ,`obtenerChatStatus(['Ada', 'Grace'])  debería retornar 'Ada y Grace están conectadas' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerChatStatus(['Ada', 'Grace', 'Marie'])  debería retornar 'Ada, Grace y 1 persona(s) más están conectadas' ", function() {
+let resultado = obtenerChatStatus(['Ada', 'Grace', 'Marie'])
+assert.equal(resultado,'Ada, Grace y 1 persona(s) más están conectadas' ,`obtenerChatStatus(['Ada', 'Grace', 'Marie'])  debería retornar 'Ada, Grace y 1 persona(s) más están conectadas' pero fue ${resultado}`);
 })
--- 00011_Multiplicar/test..js
+++ 00011_Multiplicar/test..js
@@ -1,5 +1,10 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80]", function() {
+let resultado = multiplicar(2, [5, 7, 15, 22, 40])
+assert.equal(resultado, [10, 14, 30, 44, 80],`multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80] pero fue ${resultado}`);
 })
+
+it("Si hacemos multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770]", function() {
+let resultado = multiplicar(10, [2, 5, 77])
+assert.equal(resultado, [20, 50, 770],`multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770] pero fue ${resultado}`);
+})
+
