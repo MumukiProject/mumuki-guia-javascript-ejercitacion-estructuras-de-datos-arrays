96a46095ededbfbe937c1f2365770d0436a5a569
mayra@mumuki.org <mayra@mumuki.org>
2021-03-17 15:18:39 +0000

    Mumuki Export on 2021-03-17 15:18:39 +0000

--- 00005_Gano/test..js
+++ 00005_Gano/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos invertirCaso('Ada Lovelace')  debería retornar 'aDA lOVELACE'", function() {
+let resultado = invertirCaso('Ada Lovelace') 
+assert.equal(resultado, 'aDA lOVELACE',`invertirCaso('Ada Lovelace')  debería retornar 'aDA lOVELACE' pero fue ${resultado}`);
 })
