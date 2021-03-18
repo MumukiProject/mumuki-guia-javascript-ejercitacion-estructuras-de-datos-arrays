f27b6c63551dabcbc80f5e2dc91931500692d943
mayra@mumuki.org <mayra@mumuki.org>
2021-03-18 16:39:41 +0000

    Mumuki Export on 2021-03-18 16:39:41 +0000

--- 00015_Obtener resultado/test..js
+++ 00015_Obtener resultado/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3]) debería retornar Ada", function() {
+let resultado = obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3])
+assert.equal(resultado, 'Ada',`obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3]) debería retornar 'Ada' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar Empate", function() {
+let resultado = obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9])
+assert.equal(resultado, 'Empate',`obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar 'Empate' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar Grace", function() {
+let resultado = obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3])
+assert.equal(resultado, 'Grace',`obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar 'GRace' pero fue ${resultado}`);
 })
