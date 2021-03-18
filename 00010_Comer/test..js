a7f60d6e7125ceece45b9ff26fb063614a920162
mayra@mumuki.org <mayra@mumuki.org>
2021-03-18 17:13:21 +0000

    Mumuki Export on 2021-03-18 17:13:21 +0000

--- 00010_Comer/test..js
+++ 00010_Comer/test..js
@@ -2,3 +2,9 @@ it("Si hacemos comer('🐰🥕🥬🥕🚫') debería retornar '' ", function() 
 let resultado = comer('🐰🥕🥬🥕🚫')
 assert.equal(resultado, '',`comer('🐰🥕🥬🥕🚫') debería retornar '' pero fue ${resultado}`);
 })
+
+it("Si hacemos comer('🌱🥕🌱🐰🌱🥬🌱🌱🚫🌷') debería retornar '🌱🥕🌱🌷'", function() {
+let resultado = comer('🌱🥕🌱🐰🌱🥬🌱🌱🚫🌷')
+assert.equal(resultado, '🌱🥕🌱🌷',`comer('🌱🥕🌱🐰🌱🥬🌱🌱🚫🌷') debería retornar '🌱🥕🌱🌷' pero fue ${resultado}`);
+})
+
