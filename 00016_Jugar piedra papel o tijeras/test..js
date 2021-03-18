8034f65e8eeb840bdff474eb0ea56cf9ccb43505
mayra@mumuki.org <mayra@mumuki.org>
2021-03-18 16:44:24 +0000

    Mumuki Export on 2021-03-18 16:44:24 +0000

--- 00016_Jugar piedra papel o tijeras/test..js
+++ 00016_Jugar piedra papel o tijeras/test..js
@@ -1,5 +1,20 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos jugarPiedraPapelTijeras('Ada', 'Grace', ['tijera'], ['piedra']) debería retornar Grace", function() {
+let resultado = jugarPiedraPapelTijeras('Ada', 'Grace', ['tijera'], ['piedra'])
+assert.equal(resultado, 'Grace',`jugarPiedraPapelTijeras('Ada', 'Grace', ['tijera'], ['piedra']) debería retornar 'Grace' pero fue ${resultado}`);
+})
+
+it("Si hacemos jugarPiedraPapelTijeras('Ada', 'Grace', ['papel'], ['papel']) debería retornar Empate", function() {
+let resultado = jugarPiedraPapelTijeras('Ada', 'Grace', ['papel'], ['papel'])
+assert.equal(resultado, 'Empate',`'Ada', 'Grace', ['papel'], ['papel']) debería retornar 'Empate' pero fue ${resultado}`);
+})
+
+it("Si hacemos jugarPiedraPapelTijeras('Ada','Grace',
+  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
+  ['papel', 'piedra', 'tijera', 'tijera', 'papel']) debería retornar Ada", function() {
+let resultado = jugarPiedraPapelTijeras('Ada','Grace',
+  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
+  ['papel', 'piedra', 'tijera', 'tijera', 'papel'])
+assert.equal(resultado, 'Ada',`jugarPiedraPapelTijeras('Ada','Grace',
+  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
+  ['papel', 'piedra', 'tijera', 'tijera', 'papel']) debería retornar 'Ada' pero fue ${resultado}`);
 })
