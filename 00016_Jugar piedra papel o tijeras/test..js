bde0e3047f18bdcbe8807f432cd0354cc3a0cb63
mayra@mumuki.org <mayra@mumuki.org>
2021-03-18 16:50:38 +0000

    Mumuki Export on 2021-03-18 16:50:38 +0000

--- 00016_Jugar piedra papel o tijeras/test..js
+++ 00016_Jugar piedra papel o tijeras/test..js
@@ -8,13 +8,3 @@ let resultado = jugarPiedraPapelTijeras('Ada', 'Grace', ['papel'], ['papel'])
 assert.equal(resultado, 'Empate',`'jugarPiedraPapelTijeras(Ada', 'Grace', ['papel'], ['papel']) debería retornar 'Empate' pero fue ${resultado}`);
 })
 
-it("Si hacemos jugarPiedraPapelTijeras('Ada','Grace',
-  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
-  ['papel', 'piedra', 'tijera', 'tijera', 'papel']) debería retornar Ada", function() {
-let resultado = jugarPiedraPapelTijeras('Ada','Grace',
-  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
-  ['papel', 'piedra', 'tijera', 'tijera', 'papel'])
-assert.equal(resultado, 'Ada',`jugarPiedraPapelTijeras('Ada','Grace',
-  ['piedra', 'papel', 'papel', 'piedra', 'tijera'],
-  ['papel', 'piedra', 'tijera', 'tijera', 'papel']) debería retornar 'Ada' pero fue ${resultado}`);
-})
