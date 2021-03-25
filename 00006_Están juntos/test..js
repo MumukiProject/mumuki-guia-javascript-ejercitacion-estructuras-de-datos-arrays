5e3ea125996c91d12b99927da474712598dd1523
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 19:16:58 +0000

    Mumuki Export on 2021-03-25 19:16:58 +0000

--- 00006_Están juntos/test..js
+++ 00006_Están juntos/test..js
@@ -3,9 +3,9 @@ let resultado = estanJuntos(['Sam', 'Frodo', 'Legolas'])
 assert.equal(resultado,true ,`estanJuntos(['Sam', 'Frodo', 'Legolas'])  debería retornar true pero fue ${resultado}`);
 })
 
-it("Si hacemos estanJuntos(['Aragorn', 'Frodo', 'Frodo'])  debería retornar true ", function() {
-let resultado = estanJuntos(['Aragorn', 'Frodo', 'Frodo'])
-assert.equal(resultado,true ,`estanJuntos(['Aragorn', 'Frodo', 'Frodo'])  debería retornar true pero fue ${resultado}`);
+it("Si hacemos estanJuntos(['Aragorn', 'Frodo', 'Sam'])  debería retornar true ", function() {
+let resultado = estanJuntos(['Aragorn', 'Frodo', 'Sam'])
+assert.equal(resultado,true ,`estanJuntos(['Aragorn', 'Frodo', 'Sam'])  debería retornar true pero fue ${resultado}`);
 })
 
 it("Si hacemos estanJuntos(['Sam', 'Orco', 'Frodo'])  debería retornar false ", function() {
