2cee2641fb823eea4a9ae0f0b462ae14addb2b5f
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 19:55:43 +0000

    Mumuki Export on 2021-03-25 19:55:43 +0000

--- 00007_Separar/test..js
+++ 00007_Separar/test..js
@@ -1,5 +1,9 @@
-it("Si hacemos separar(['🐶','🐱','🐶','🐱','🐱','🐶','🐶']) debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱']", function() {
+/*it("Si hacemos separar(['🐶','🐱','🐶','🐱','🐱','🐶','🐶']) debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱']", function() {
 let resultado = separar([🐶,🐱,🐶,🐱,🐱,🐶,🐶])
 assert.equal(resultado, [🐶,🐶,🐶,🐶,🐱,🐱,🐱],`separar debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱'] pero fue ${resultado}`);
 })
-
+*/
+it("Si hacemos separar([]) debería retornar []", function() {
+let resultado = separar([1,2,1,2,2,1])
+assert.equal(resultado, [1,1,1,2,2,2],`separar debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱'] pero fue ${resultado}`);
+})
