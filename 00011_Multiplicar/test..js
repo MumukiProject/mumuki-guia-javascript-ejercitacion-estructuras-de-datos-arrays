a7e66fe6100f04e4f894a4d3c9e1adcc9b173f83
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 20:35:45 +0000

    Mumuki Export on 2021-03-25 20:35:45 +0000

--- 00011_Multiplicar/test..js
+++ 00011_Multiplicar/test..js
@@ -2,11 +2,11 @@
 
 it("Si hacemos multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80]", function() {
 let resultado = multiplicar(2, [5, 7, 15, 22, 40])
-assert.deepEqual([20, 50, 770],resultado ,`multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80] pero fue ${resultado}`);
+assert.deepEqual(resultado,[10, 14, 30, 44, 80] ,`multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80] pero fue ${resultado}`);
 })
 
 it("Si hacemos multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770]", function() {
 let resultado = multiplicar(10, [2, 5, 77])
-assert.deepEqual([10, 14, 30, 44, 80],resultado ,`multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770] pero fue ${resultado}`);
+assert.deepEqual(resultado,[20, 50, 770] ,`multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770] pero fue ${resultado}`);
 })
 
