6179650cf1d8557aff744c4cf2fcf8b88a496f7a
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 20:16:48 +0000

    Mumuki Export on 2021-03-25 20:16:48 +0000

--- 00011_Multiplicar/test..js
+++ 00011_Multiplicar/test..js
@@ -1,13 +1,13 @@
-let solucion = 10, 14, 30, 44, 80
-let solucionDos = 20, 50, 770
+let solucion = [10, 14, 30, 44, 80]
+let solucionDos = [20, 50, 770]
 
 it("Si hacemos multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80]", function() {
 let resultado = multiplicar(2, [5, 7, 15, 22, 40])
-assert.equal(resultado,solucion ,`multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80] pero fue ${resultado}`);
+assert.equal(resultado,[solucion] ,`multiplicar(2, [5, 7, 15, 22, 40]) debería retornar [10, 14, 30, 44, 80] pero fue ${resultado}`);
 })
 
 it("Si hacemos multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770]", function() {
 let resultado = multiplicar(10, [2, 5, 77])
-assert.equal(resultado,solucionDos ,`multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770] pero fue ${resultado}`);
+assert.equal(resultado,[solucionDos] ,`multiplicar(10, [2, 5, 77]) debería retornar [20, 50, 770] pero fue ${resultado}`);
 })
 
