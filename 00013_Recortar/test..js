2757f65781fea464a3d8f9d5649af219a09a8252
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 15:03:04 +0000

    Mumuki Export on 2021-03-26 15:03:04 +0000

--- 00013_Recortar/test..js
+++ 00013_Recortar/test..js
@@ -1,9 +1,9 @@
 it("Si hacemos recortar(4, ['elefante', 'dinosaurio'. 'chocolate', 'avion', 'america']) debería retornar ['elef', 'dino', 'choc', 'avio', 'amer']", function() {
 let resultado = recortar(4, ['elefante', 'dinosaurio', 'chocolate', 'avion', 'america'])
-assert.equal(resultado, ['elef', 'dino', 'choc', 'avio', 'amer'],`recortar(4, ['elefante', 'dinosaurio'. 'chocolate', 'avion', 'america']) debería retornar ['elef', 'dino' 'choc', 'avio', 'amer'] pero fue ${resultado}`);
+assert.deepEqual(resultado, ['elef', 'dino', 'choc', 'avio', 'amer'],`recortar(4, ['elefante', 'dinosaurio'. 'chocolate', 'avion', 'america']) debería retornar ['elef', 'dino' 'choc', 'avio', 'amer'] pero fue ${resultado}`);
 })
 
 it("Si hacemos recortar(1, ['algoritmo', 'bug', 'compilador']) debería retornar ['a', 'b','c']", function() {
 let resultado = recortar(1, ['algoritmo', 'bug', 'compilador'])
-assert.equal(resultado, ['a', 'b', 'c'],`recortar(1, ['algoritmo', 'bug', 'compilador']) debería retornar ['a', 'b', 'c'] pero fue ${resultado}`);
+assert.deepEqual(resultado, ['a', 'b', 'c'],`recortar(1, ['algoritmo', 'bug', 'compilador']) debería retornar ['a', 'b', 'c'] pero fue ${resultado}`);
 })
