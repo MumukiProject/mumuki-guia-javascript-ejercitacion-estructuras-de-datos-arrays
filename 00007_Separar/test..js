66246cbb0c50b79e816534e39775b7e2f51e3ad7
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 14:40:56 +0000

    Mumuki Export on 2021-03-26 14:40:56 +0000

--- 00007_Separar/test..js
+++ 00007_Separar/test..js
@@ -1,9 +1,5 @@
-/*it("Si hacemos separar(['🐶','🐱','🐶','🐱','🐱','🐶','🐶']) debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱']", function() {
-let resultado = separar([🐶,🐱,🐶,🐱,🐱,🐶,🐶])
-assert.equal(resultado, [🐶,🐶,🐶,🐶,🐱,🐱,🐱],`separar debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱'] pero fue ${resultado}`);
+it("Si hacemos separar(['🐶','🐱','🐶','🐱','🐱','🐶','🐶']) debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱']", function() {
+let resultado = separar(['🐶','🐱','🐶','🐱','🐱','🐶','🐶'])
+assert.deepEqual(resultado, ['🐶','🐶','🐶','🐶','🐱','🐱','🐱'],`separar debería retornar ['🐶','🐶','🐶','🐶','🐱','🐱','🐱'] pero fue ${resultado}`);
 })
-*/
-it("Si hacemos separar([1,2,1,2,2,1]) debería retornar [1,1,1,2,2,2]", function() {
-let resultado = separar([1,2,1,2,2,1])
-assert.equal(resultado, [1,1,1,2,2,2],`separar debería retornar [1,1,1,2,2,2] pero fue ${resultado}`);
-})
+
