aa7b1b1beae0be32cc36158e43fbcdf8251e0659
mayra@mumuki.org <mayra@mumuki.org>
2021-03-30 14:10:41 +0000

    Mumuki Export on 2021-03-30 14:10:40 +0000

--- 00010_Comer/test..js
+++ 00010_Comer/test..js
@@ -5,7 +5,7 @@ assert.deepEqual(resultado, '',`comer debería retornar '' pero fue ${resultado}
 
 it("Si hacemos comer('🌱🥕🌱🐰🌱🥬🌱🌱🚫🌷') debería retornar '🌱🥕🌱🌷'", function() {
 let resultado = comer('🌱🥕🌱🐰🌱🥬🌱🌱🚫🌷')
-assert.equal(resultado,  '🌱🥕🌱🌷' ,`comer debería retornar '🌱🥕🌱🌷' pero fue ${resultado}`);
+assert.deepEqual(resultado,  '🌱🥕🌱🌷' ,`comer debería retornar '🌱🥕🌱🌷' pero fue ${resultado}`);
 })
 
 
